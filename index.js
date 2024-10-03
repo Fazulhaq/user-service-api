c41bf01f051bbd761388b33db15fff32a873ab06
Fazulhaq Amin <128462337+Fazulhaq@users.noreply.github.com>
2024-10-03 09:20:45 +0430

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.json([
+        {
+            'firstName': "Ahamd",
+            'lastName': "Khan"
+        },
+        {
+            'firstName': "Mohammad",
+            'lastName': "Jan"
+        },
+        {
+            'firstName': "Karim",
+            'lastName': "Rahimi"
+        },
+    ])
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
